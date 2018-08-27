b5073cdea593240c12fee844bd07de0e3e5a92fe
Rex Djere <rdjere@fedora.desktop2>
2018-08-26 21:44:41 -0400

    Started C version.
    Signed-off-by: Rex Djere <rdjere@fedora.desktop2>

---
+++ versions/version_c/Linux_newusers_file_creator.c
@@ -0,0 +1,16 @@
+/* 	Linux Newusers File Creator: creates a file needed to batch add 
+ * 	users with Linux "newusers" command.               
+ *	Each line in output file holds data for 1 user.                                                                    
+ *	Author: Rex Djere.                                                                                                 
+ *	2018-Present, Rex Djere.                                                                                         
+ *	License: GPL version 3 or later.                                                                                   
+ *	Requirements: C11 or later.                                                                                     
+ *	Version: Friday 08/17/18 08:11:41 PM                                                                              
+ *	Generate version: date '+%A %D %X' 
+ */
+
+/* output line format: <Username>:<Password>:<UID>:<GID>:<User Info>:<Home Directory>:<Default Shell> */  
+
+/* library header files */
+   #include <stdio.h                                                                       
+
